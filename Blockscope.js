cdbf104a583ceb7c88503c996ba821dd968c8e86
P K KAVYA <132293399+Kavya-918@users.noreply.github.com>
2025-03-02 12:52:23 +0530

    Create Blockscope.js

---
+++ Blockscope.js
@@ -0,0 +1,20 @@
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a); 
+    console.log(b); 
+    console.log(c); 
+}
+    console.log(a); 
+    console.log(b); 
+    console.log(c); 
+
+//output as in console
+10
+Blockscope.js:6 20
+Blockscope.js:7 30
+Blockscope.js:9 10
+Blockscope.js:10  Uncaught ReferenceError: b is not defined
+    at Blockscope.js:10:17
+Hello.html:44 Live reload enabled.
